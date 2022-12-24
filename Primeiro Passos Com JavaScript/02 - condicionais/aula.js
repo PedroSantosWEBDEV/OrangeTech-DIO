1039ef3fdcbccdb95636e6fe99c4e6ae69a8f88d
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-12-24 09:14:36 -0300

    Update aula.js

--- Primeiro Passos Com JavaScript/02 - condicionais/aula.js
+++ Primeiro Passos Com JavaScript/02 - condicionais/aula.js
@@ -1,15 +1,20 @@
 
 
 
-const numero = 3;
+const numero = 35;
 
-const numeroPar = (numero % 2) === 0;
+const numeroPar = (numero % 5) === 0;
 // console.log(numeroPar);
 
-if (numeroPar) {
-    console.log('Par')
-}
+if (numero === 0) {
 
-if (!numeroPar) {
-    console.log('Impar')
+    console.log('O numero é invalido');
+
+} else {
+
+    if (numeroPar) {
+        console.log('Sim')
+    } else {
+        console.log('Nao')
+    }
 }
