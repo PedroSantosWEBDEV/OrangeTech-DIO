28fd07f9f3734cdda20a954d3c08a36158e77e02
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-12-24 09:42:49 -0300

    desafio

--- Primeiro Passos Com JavaScript/02 - condicionais/segundo_desafio.js
+++ Primeiro Passos Com JavaScript/02 - condicionais/segundo_desafio.js
@@ -12,18 +12,35 @@
 
 // Primeira Solução
 
+// const precoGasolina = 5.25;
+// const precoEtanol = 4.94;
+// const kmPorLitros = 30;
+// const distancia = 100;
+// const tipoCombustivel = 'etanol';
+
+// if(tipoCombustivel === 'gasolina'){
+// const litrosConsumidos = distancia/kmPorLitros;
+// const valorGasto = litrosConsumidos * precoGasolina;
+// console.log(valorGasto.toFixed(2));
+// }else (tipoCombustivel === 'etanol'){
+//     const litrosConsumidos = distancia/kmPorLitros;
+//     const valorGasto = litrosConsumidos * precoEtanol;
+//     console.log(valorGasto.toFixed(2));
+// }
+
+// Segunda Solução
+
 const precoGasolina = 5.25;
 const precoEtanol = 4.94;
 const kmPorLitros = 30;
-const distancia = 1580;
-const tipoCombustivel = 'etanol';
+const distancia = 100;
+const tipoCombustivel = 'gasolina';
+const litrosConsumidos = distancia / kmPorLitros;
 
-if(tipoCombustivel == 'gasolina'){
-const litrosConsumidos = distancia/kmPorLitros;
-const valorGasto = litrosConsumidos * precoGasolina;
-console.log(valorGasto.toFixed(2));
-}else if(tipoCombustivel == 'etanol'){
-    const litrosConsumidos = distancia/kmPorLitros;
+if (tipoCombustivel === 'gasolina') {
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
+} else {
     const valorGasto = litrosConsumidos * precoEtanol;
     console.log(valorGasto.toFixed(2));
 }
