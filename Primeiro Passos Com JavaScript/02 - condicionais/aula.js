e82fab427b96dfaa3946bafb73cf9428dae87022
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-12-24 09:18:06 -0300

    Update aula.js

--- Primeiro Passos Com JavaScript/02 - condicionais/aula.js
+++ Primeiro Passos Com JavaScript/02 - condicionais/aula.js
@@ -1,20 +1,17 @@
 
 
 
-const numero = 35;
+const numero = 5;
 
-const numeroPar = (numero % 5) === 0;
+const numeroDivisivelPor5 = (numero % 5) === 0;
 // console.log(numeroPar);
 
 if (numero === 0) {
-
     console.log('O numero é invalido');
-
+} else if (numero === 5) {
+    console.log('O numero é invalido');
+} else if (numeroDivisivelPor5) {
+    console.log('Sim')
 } else {
-
-    if (numeroPar) {
-        console.log('Sim')
-    } else {
-        console.log('Nao')
-    }
+    console.log('Nao')
 }
