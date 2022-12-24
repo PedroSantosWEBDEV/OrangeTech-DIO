c87903d2baff9268c7acc7f1f8d691b7d7d0c363
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-12-24 09:33:10 -0300

    Update segundo_desafio.js

--- Primeiro Passos Com JavaScript/02 - condicionais/segundo_desafio.js
+++ Primeiro Passos Com JavaScript/02 - condicionais/segundo_desafio.js
@@ -9,3 +9,21 @@
 //  3 - Distancia em km da viagem;
 
 // Imprima no console o valor que sera gasto para realizar esta viagem.
+
+// Primeira Solução
+
+const precoGasolina = 5.25;
+const precoEtanol = 4.94;
+const kmPorLitros = 30;
+const distancia = 1580;
+const tipoCombustivel = 'etanol';
+
+if(tipoCombustivel == 'gasolina'){
+const litrosConsumidos = distancia/kmPorLitros;
+const valorGasto = litrosConsumidos * precoGasolina;
+console.log(valorGasto.toFixed(2));
+}else if(tipoCombustivel == 'etanol'){
+    const litrosConsumidos = distancia/kmPorLitros;
+    const valorGasto = litrosConsumidos * precoEtanol;
+    console.log(valorGasto.toFixed(2));
+}
