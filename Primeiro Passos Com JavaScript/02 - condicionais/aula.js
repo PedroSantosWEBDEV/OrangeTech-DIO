9090e90f0c7a0f7a7036b1bc4a6d93b98d48987c
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-12-24 09:23:53 -0300

    desafio

--- Primeiro Passos Com JavaScript/02 - condicionais/aula.js
+++ Primeiro Passos Com JavaScript/02 - condicionais/aula.js
@@ -1,17 +1,21 @@
 
 
 
-const numero = 5;
+const numero = 2;
+const isNumeroPar =  (numero % 2) === 0;
+// const numeroDivisivelPor5 = (numero % 5) === 0;
+// // console.log(numeroPar);
 
-const numeroDivisivelPor5 = (numero % 5) === 0;
-// console.log(numeroPar);
+// if (numero === 0) {
+//     console.log('O numero é invalido');
+// } else if (numero === 5) {
+//     console.log('O numero é invalido');
+// } else if (numeroDivisivelPor5) {
+//     console.log('Sim')
+// } else {
+//     console.log('Nao')
+// }
 
-if (numero === 0) {
-    console.log('O numero é invalido');
-} else if (numero === 5) {
-    console.log('O numero é invalido');
-} else if (numeroDivisivelPor5) {
-    console.log('Sim')
-} else {
-    console.log('Nao')
+if(isNumeroPar){
+    console.log('O numero é par');
 }
---
+++ Primeiro Passos Com JavaScript/02 - condicionais/segundo_desafio.js
@@ -0,0 +1,11 @@
+
+// Faça um programa para calcular o valor de uma viagem
+
+// Voce tera 5 variaveis. Sendo elas:
+//  1 - Preço do etanol;
+//  2 - Preço da gasolina;
+//  3 - O tipo de combustivel que está no seu carro;
+//  2 - Gasto medio de combustivel de carro por km;
+//  3 - Distancia em km da viagem;
+
+// Imprima no console o valor que sera gasto para realizar esta viagem.
